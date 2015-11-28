592c3e8597ad9c4e0ee59cb0957853cac498274d
Robbie Speed <robert.y.speed@gmail.com>
2015-11-27 22:28:20 -0400

    added boilerplate

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -5,6 +5,13 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/bourbon/app/assets/stylesheets',
+        'bower_components/neat/app/assets/stylesheets',
+        'bower_components/normalize-css',
+      ],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
