9926031b13666835a5c55a4913ebcc9fff1ad82e
Robbie Speed <robert.y.speed@gmail.com>
2015-11-28 15:24:16 -0400

    fix inverse on game

--- client/app/models/game.js
+++ client/app/models/game.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  awayTeam: DS.belongsTo('team'),
+  homeTeam: DS.belongsTo('team'),
+  stadium: DS.belongsTo('stadium'),
+  score: DS.attr(),
+  attendance: DS.attr('number'),
+  soldout: DS.attr('boolean'),
+  date: DS.attr('date'),
 });
--- client/app/models/team.js
+++ client/app/models/team.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   twitter: DS.attr('string'),
   fanScore: DS.attr('number'),
-  games: DS.hasMany('game'),
+  games: DS.hasMany('game', {inverse: null}),
   fans: DS.hasMany('fan'),
 });
