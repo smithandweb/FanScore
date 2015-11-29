3770b3bd5f62fa58857d2ca3efc32de1a1294223
Josh Smith <smithandweb@gmail.com>
2015-11-29 12:50:04 -0400

    team page

---
+++ client/app/controllers/team.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  fans: Ember.computed('model.fans', function () {
+    let fans;
+    if (this.get('model.fans')) {
+      fans = this.get('model.fans').map((item) => {
+        return {fanScore: item.get('fanScore').objectAt(0), name: item.get('name')}
+      });
+    }
+    return fans;
+  }),
+});
